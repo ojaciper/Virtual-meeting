5a46e3faf53a47a6d4b1441bd49e1ddea5dc9a84
peter osas <osaspeter6@gmail.com>
2025-01-07 06:40:56 +0100

    add toggle to the button

--- lib/src/feature/calender/widget/custom_bottom_sheet.dart
+++ lib/src/feature/calender/widget/custom_bottom_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:virtual_meeting/src/feature/calender/widget/custom_button.dart';
 
 class CustomBottomSheet extends StatefulWidget {
   const CustomBottomSheet({super.key});
@@ -9,7 +8,8 @@ class CustomBottomSheet extends StatefulWidget {
 }
 
 class _CustomBottomSheetState extends State<CustomBottomSheet> {
-  final bool _value = true;
+  bool _isPressed = false;
+  bool isPressed = true;
 
   @override
   Widget build(BuildContext context) {
@@ -22,37 +22,40 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // InkWell(
-          //   onTap: () {},
-          //   child: Container(
-          //     alignment: Alignment.center,
-          //     height: 40,
-          //     width: 85.5,
-          //     decoration: BoxDecoration(
-          //         borderRadius: BorderRadius.circular(10),
-          //         border: Border.all(
-          //           color: Color(0xFFFFFFFF),
-          //           width: 1,
-          //         )),
-          //     child: Text(
-          //       'Back',
-          //       style: TextStyle(
-          //         color: Color(0xFFFCFCFC),
-          //         fontSize: 14,
-          //         fontWeight: FontWeight.w600,
-          //         fontFamily: 'poppins',
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          CustomButton(
-            text: "Back",
-            value: _value,
-            onTap: (value) {
+          InkWell(
+            onTap: () {
               setState(() {
-                value = !value;
+                isPressed = false;
+                _isPressed = true;
               });
             },
+            child: Container(
+              alignment: Alignment.center,
+              height: 40,
+              width: 85.5,
+              decoration: BoxDecoration(
+                  color: _isPressed == true
+                      ? Color(0xFFFFDD99)
+                      : Colors.transparent,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: _isPressed == false
+                        ? Color(0xFFFFFFFF)
+                        : Colors.transparent,
+                    width: 1,
+                  )),
+              child: Text(
+                'Back',
+                style: TextStyle(
+                  color: _isPressed == true
+                      ? Color(0xFF616973)
+                      : Color(0xFFFCFCFC),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'poppins',
+                ),
+              ),
+            ),
           ),
           Text(
             '3 steps to submit',
@@ -63,35 +66,41 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
               fontFamily: 'Poppins',
             ),
           ),
-          // InkWell(
-          //   onTap: () {},
-          //   child: Container(
-          //     alignment: Alignment.center,
-          //     height: 40,
-          //     width: 85.5,
-          //     decoration: BoxDecoration(
-          //       color: Color(0xFFFFDD99),
-          //       borderRadius: BorderRadius.circular(10),
-          //     ),
-          //     child: Text(
-          //       'Next',
-          //       style: TextStyle(
-          //         color: Color(0xFF616973),
-          //         fontSize: 14,
-          //         fontWeight: FontWeight.w600,
-          //         fontFamily: 'poppins',
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          CustomButton(
-            text: "Next",
-            value: _value,
-            onTap: (value) {
+          InkWell(
+            onTap: () {
               setState(() {
-                value = !_value;
+                isPressed = true;
+                _isPressed = false;
               });
             },
+            child: Container(
+              alignment: Alignment.center,
+              height: 40,
+              width: 85.5,
+              decoration: BoxDecoration(
+                color: _isPressed == false
+                    ? Color(0xFFFFDD99)
+                    : Colors.transparent,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: _isPressed == true
+                      ? Color(0xFFFFFFFF)
+                      : Colors.transparent,
+                  width: 1,
+                ),
+              ),
+              child: Text(
+                'Next',
+                style: TextStyle(
+                  color: _isPressed == false
+                      ? Color(0xFF616973)
+                      : Color(0xFFFCFCFC),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'poppins',
+                ),
+              ),
+            ),
           ),
         ],
       ),
